722be8b442b51cda58853c08dd1779092ba91ca9
Ousmane Sakho <genieouzog@gmail.com>
2022-05-16 19:19:51 +0000

    data bbinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo';
+  title = "TODO";
+  taskTitle = "";
+  taskStatus = "";
+  tasks = [
+    { title: "Task 1", id: 9, status: "In Progress"},
+    { title: "Task 2", id: 27, status: "To Do" },
+    { title: "Task 3", id: 48, status: "In Progress" },
+    { title: "Task 4", id: 33, status: "Done" },
+    { title: "Task 5", id: 42, status: "To Do" }
+  ];
+  statusSelected = null;
+
+  deleteTask(taskId: number) {
+    const taskIndex = this.tasks.findIndex((task) => task.id === taskId);
+    this.tasks.splice(taskIndex, 1);
+  }
+
+  addTask() {
+    const lastIndex = this.tasks.length - 1;
+    const id = this.tasks[lastIndex].id + 1;
+    this.tasks.push({ title: this.taskTitle,  id, status: this.taskStatus });
+    this.taskTitle = "";
+  }
+
+  filterTask() {
+    if(this.statusSelected === null) {
+      return this.tasks;
+    }
+    return this.tasks.filter((task) => this.statusSelected === task.status);
+  }
 }
